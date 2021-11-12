2ae19521a84b2951570cbc86ba074265be97843e
Pavithra-Marie <93261347+Pavithra-Marie@users.noreply.github.com>
2021-11-12 16:19:27 +0530

    Create pusher.js

---
+++ client/pusher.js
@@ -0,0 +1,8 @@
+{
+    "appId":"1294679",
+    "key":"08449457017388223cef",
+    "secret":"3ab7cff6a937647c71e2",
+    "cluster":"ap2",
+    "encrypted":true,
+    "restServer":"http://192.168.43.159:4000"
+}
